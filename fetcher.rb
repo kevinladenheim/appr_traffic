6e19c5be369b2af574f798c20ce1f786f73b2571
kpladenheim@gmail.com <kpladenheim@gmail.com>
2012-05-19 00:09:09 -0400

    added Bing data, pulled keys out

--- fetcher.rb
+++ fetcher.rb
@@ -3,42 +3,71 @@ require "nokogiri"
 
 module Fetchers
    class Traffic < Base
-    BASE_URL = "http://www.mapquestapi.com"
-    API_KEY = "Fmjtd%7Cluua2d6bl9%2Can%3Do5-hrr0d"
+    MQ_BASE_URL = "http://www.mapquestapi.com"
+    BING_BASE_URL = "http://dev.virtualearth.net"
 
+    def initialize(lat1, lon1, lat2, lon2)
+      @lat1 = lat1; 
+      @lon1 = lon1; 
+      @lat2 = lat2; 
+      @lon2 = lon2;
 
-    def fetch
-      http_request("#{BASE_URL}/traffic/v1/incidents?key=#{API_KEY}&callback=handleIncidentsResponse&boundingBox=39.503136,\
--76.887259,39.077998,-76.337942&filters=construction,incidents&inFormat=kvp&outFormat=xml") do |body|
+      # is this the perfect place to read in keys or an evil place?
+      # to do: make each line easily parseable key/value pair so order doesn't matter
+      keys = IO.readlines("keys.txt")
+
+      @mq_api_key = keys[0].strip!
+      @bing_api_key = keys[1].strip!      
+    end
+
+    def fetch_mq
+      http_request("#{MQ_BASE_URL}/traffic/v1/incidents?key=#{@mq_api_key}&callback=handleIncidentsResponse&boundingBox=#{@lat1},\
+#{@lon1},#{@lat2},#{@lon2}&filters=construction,incidents&inFormat=kvp&outFormat=xml") do |body|
         doc = Nokogiri::XML(body) 
       
-        #doc = Nokogiri::XML(File.open(body)) do |config|
-        #  config.options = Nokogiri::XML::ParseOptions.STRICT | Nokogiri::XML::ParseOptions.NOENT
-        #end
-
         severity = doc.xpath("//severity")
-        fullDesc = doc.xpath("//fullDesc")        
-        
-        # this is bad, regexp would be better, what's best?
+        fullDesc = doc.xpath("//fullDesc")
 
         print "\n\n"
 
+        descs = []
+        severities = []
+
         for i in 0..fullDesc.length - 1
-          severities = severity[i].to_s.sub("<severity>","").sub("</severity>","")
-          full_descs = fullDesc[i].to_s.sub("<fullDesc>", "").sub("</fullDesc>", "")
-
-          print "%2d==> (%s) " % [i, severities]
-          print full_descs, "\n"
+          # this is bad, regexp would be better, what's best?
+          severities[i] = severity[i].to_s.sub("<severity>","").sub("</severity>","")
+          descs[i] = fullDesc[i].to_s.sub("<fullDesc>", "").sub("</fullDesc>", "")
         end
-                
-        @data = {
-          incidents: fullDesc.length,
-        }
+        
+        # is data a keyword?
+        @data = { mq_incidents: fullDesc.length, mq_descs: descs, mq_severities: severities}
+        end 
+     end
 
+
+      def fetch_bing
+        http_request("#{BING_BASE_URL}/REST/V1/Traffic/Incidents/#{@lat1},#{@lon1},#{@lat2},#{@lon2}/?&o=xml&key=#{@bing_api_key}") do |body|
+        doc = Nokogiri::XML(body) 
+
+        # the mapquest data didn't need a fully qualified XML namespace, 
+        # maybe because it didn't define one like the bing data does
+        severity = doc.xpath("//xmlns:Severity")
+        fullDesc = doc.xpath("//xmlns:Description")        
+
+        descs = []  
+        severities = []
+
+        for i in 0..fullDesc.length - 1
+          severities[i] = severity[i].to_s.sub("<Severity>","").sub("</Severity>","")
+          descs[i] = fullDesc[i].to_s.sub("<Description>", "").sub("</Description>", "")
+        end
+
+        @data = { bing_incidents: fullDesc.length, bing_descs: descs, bing_severities: severities}
+      end
       end
     end
-  end
-end
+ end
+
 
 
 
--- traffic.rb
+++ traffic.rb
@@ -3,23 +3,42 @@
 require_relative "fetcher"
 
 
-unless ARGV.length == 0
-  print "No arguments\n\n"
+unless ARGV.length == 4
+  print "Four arguments: lat1 lon1 lat2 lon2\n"
+  print "Example: 39.0 -77.0 40.0 -76.0\n\n"
   exit
 end
 
-@sym = {word: ARGV[0]}
-report = Fetchers::Traffic.new(@sym)
+@lat1 = ARGV[0]
+@lon1 = ARGV[1]
+@lat2 = ARGV[2]
+@lon2 = ARGV[3]
 
-report.fetch
-message = report.message
+report = Fetchers::Traffic.new(@lat1, @lon1, @lat2, @lon2)
 
-print "\n\nHTTP Status: ", message, "\n\n"
-
-if message.include? '404'
-  print "Unknown Word\n\n"
-  exit
+def dump(incidents, severities, descriptions)
+  for i in 0..incidents - 1
+    print "%2d==> (%s) " % [i, severities[i]]
+    print descriptions[i], "\n"
+  end
 end
 
-print "Incidents:     ", report.data[:incidents], "\n"
+report.fetch_mq
+dump(report.data[:mq_incidents], report.data[:mq_severities], report.data[:mq_descs]) 
 print "\n\n"
+
+report.fetch_bing
+dump(report.data[:bing_incidents], report.data[:bing_severities], report.data[:bing_descs]) 
+print "\n\n"
+
+#message = report.message
+
+#print "\n\nHTTP Status: ", message, "\n\n"
+
+#if message.include? '404'
+#  print "Unknown Word\n\n"
+
+#end
+
+#print "Mapquest desc: ", report.descs[1]
+
